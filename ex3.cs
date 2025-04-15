d726fbd9098610b3bddaa301f3c750b6fab8713e
guirocha <guilherme.rocha.7@outlook.com>
2025-04-14 23:26:15 -0300

    parte1

---
+++ ex3.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Verificação de palíndromo: Digite uma palavra ou frase.....");
+        string input = Console.ReadLine();
+
+        
+    }
+}
