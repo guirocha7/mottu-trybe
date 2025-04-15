77e360b0cd32d49d46372d490ca797d4957b5e0f
guirocha <guilherme.rocha.7@outlook.com>
2025-04-14 23:22:04 -0300

    parte3

--- ex2.cs
+++ ex2.cs
@@ -1,5 +1,4 @@
 using System;
-
 class JogoAdivinhacao
 {
     static void Main(string[] args)
@@ -7,7 +6,7 @@ class JogoAdivinhacao
         Random random = new Random();
         int numeroAdivinhar = random.Next(1, 1001);
 
-         Console.WriteLine("Exercício 2:Jogo de Adivinhação");
+        Console.WriteLine("Exercício 2:Jogo de Adivinhação");
         Console.WriteLine("Você vai tentar adivinhar um número entre 1 e 1000.");
 
 
@@ -22,3 +21,35 @@ class JogoAdivinhacao
             {
                 Console.WriteLine("Parabéns! Você adivinhou o número!");
                 adivinhou = true;
+            }
+            else if (tentativa < numeroAdivinhar)
+            {
+                Console.WriteLine("O número é maior que sua tentativa.");
+            }
+            else
+            {
+                Console.WriteLine("O número é menor que sua tentativa.");
+            }
+
+            int diferenca = Math.Abs(tentativa - numeroAdivinhar);
+
+            if (!adivinhou)
+            {
+                if (diferenca > 100)
+                {
+                    Console.WriteLine("Você está muito distante.");
+                }
+                else if (diferenca > 50)
+                {
+                    Console.WriteLine("Você está distante.");
+                }
+                else
+                {
+                    Console.WriteLine("Você está perto.");
+                }
+            }
+        }
+
+        Console.WriteLine("Obrigado por jogar!");
+    }
+}
