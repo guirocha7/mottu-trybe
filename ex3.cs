f52fd1dfc4be0c1a4a5f654d8a7e515a19be5c2b
guirocha <guilherme.rocha.7@outlook.com>
2025-04-14 23:32:25 -0300

    parte2

--- ex3.cs
+++ ex3.cs
@@ -4,9 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Verificação de palíndromo: Digite uma palavra ou frase.....");
+        Console.WriteLine("Verificação de palíndromo: Digite uma palavra.....");
         string input = Console.ReadLine();
 
-        
+        if (EhPalindromo(input))
+        {
+            Console.WriteLine("A palavra é um palíndromo!");
+        }
+        else
+        {
+            Console.WriteLine("A palavra não é um palíndromo.");
+        }
     }
-}
+
