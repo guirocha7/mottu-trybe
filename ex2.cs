d464b0e1e7811e3b91d40dce73b3ceb2b11ca82e
guirocha <guilherme.rocha.7@outlook.com>
2025-04-14 23:17:17 -0300

    parte2

--- ex2.cs
+++ ex2.cs
@@ -1,23 +1,24 @@
 using System;
 
-class Ex2JogoAdivinhacao
+class JogoAdivinhacao
 {
     static void Main(string[] args)
     {
         Random random = new Random();
         int numeroAdivinhar = random.Next(1, 1001);
 
-        Console.WriteLine("Bem-vindo ao Jogo de Adivinhação!");
-        Console.WriteLine("Tente adivinhar o número entre 1 e 1000.");
+         Console.WriteLine("Exercício 2:Jogo de Adivinhação");
+        Console.WriteLine("Você vai tentar adivinhar um número entre 1 e 1000.");
+
 
         bool adivinhou = false;
 
         while (!adivinhou)
         {
-            Console.Write("Digite sua tentativa: ");
+            Console.Write("Teste a sua sorte, digite sua tentativa: ");
             int tentativa = int.Parse(Console.ReadLine());
 
-            
-        }
-    }
-}
+            if (tentativa == numeroAdivinhar)
+            {
+                Console.WriteLine("Parabéns! Você adivinhou o número!");
+                adivinhou = true;
