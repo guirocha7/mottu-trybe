46b35f77d0dd8b1803b3e6e445c23d2cb5514dfc
guirocha <guilherme.rocha.7@outlook.com>
2025-04-14 23:39:39 -0300

    parte3/ex3

--- ex3.cs
+++ ex3.cs
@@ -13,7 +13,32 @@ class Program
         }
         else
         {
-            Console.WriteLine("A palavra não é um palíndromo.");
+            Console.WriteLine("A palavra não é um palíndromo!");
         }
     }
 
+    static string LimparTexto(string texto)
+    {
+        return texto.Replace(" ", "").ToLower();
+    }
+
+    static bool EhPalindromo(string texto)
+    {
+        string textolimpo = LimparTexto(texto);
+
+        int inicio = 0;
+        int fim = textolimpo.Length - 1;
+
+        while (inicio < fim)
+        {
+            if (textolimpo[inicio] != textolimpo[fim])
+            {
+                return false;
+            }
+            inicio++;
+            fim--;
+        }
+
+        return true;
+    }
+}
